e29b90f951560de0704893f2e202f2a3ccdc2303
jesperhansen17 <hansen.jesper17@gmail.com>
2015-10-13 09:48:16 +0200

    Added a simple XML file for ItemFragment

---
+++ app/src/main/java/mah/bidme/ItemFragment.java
@@ -0,0 +1,30 @@
+package mah.bidme;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ItemFragment extends Fragment {
+
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_item, container, false);
+        return view;
+    }
+
+}
