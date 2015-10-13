e7c6216780103c67a57fe1f908b82df62e9ea4b8
Khaa05 <ha.kevin2012@gmail.com>
2015-10-13 14:09:17 +0200

    Add Constants class for connecting Firebase database

--- app/src/main/java/mah/bidme/Constants.java
+++ app/src/main/java/mah/bidme/Constants.java
@@ -6,4 +6,7 @@ import com.firebase.client.Firebase;
  * Created by Kha on 13/10/15.
  */
 public class Constants {
+    //Since this is static it will be instansiated  at startup of the App
+    //Use static for variables that you want to reach from anywhere in the app...
+    public static Firebase myFirebaseRef = new Firebase("https://biddme.firebaseio.com/");
 }
