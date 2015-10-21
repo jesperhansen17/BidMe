2eac236cbf273639e84aba9077ce2eaed9641da2
Khaa05 <ha.kevin2012@gmail.com>
2015-10-21 17:04:02 +0200

    Class Items & Users created

--- app/src/main/java/mah/bidme/model/Item.java
+++ app/src/main/java/mah/bidme/model/Item.java
@@ -1,44 +1,58 @@
 package mah.bidme.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by JesperHansen on 2015-10-14.
  */
 public class Item {
-    private int id;
+    private String id;
     private String title;
     private String description;
-    private int price;
-    private int idSeller;
-    private int idBuyer;
-    private List<User> listBid;
+    private int currentPrice;
+    private int startedPrice;
+    private String idSeller;
+    private String idBuyer;
+    private List<HashMap<String, Object>> listBid;
 
-    public Item(String title, String description, int price, int idSeller) {
+    public Item(String title, String description, int price, String idSeller) {
         this.title = title;
         this.description = description;
-        this.price = price;
+        this.startedPrice = price;
+        this.currentPrice = price;
         this.idSeller = idSeller;
-        listBid = new ArrayList<User>();
+        listBid = new ArrayList<HashMap<String, Object>>();
     }
 
     /**
      * Get the winner of the bidding
      * @return User
      */
-    public User getWinner(){
+    public HashMap<String, Object> getWinner(){
         return listBid.get(listBid.size() - 1);
+    }
+
+    /**
+     * Get the winner of the bidding
+     * @return User
+     */
+    public void setWinner(String idWinner){
+        this.idBuyer = idWinner;
     }
 
     /**
      * Return the bidding historical
      * @return List<User>
      */
-    public List<User> getHistoricalBids(){
+    public List<HashMap<String, Object>> getHistoricalBids(){
         return listBid;
     }
 
+    public String getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
@@ -48,15 +62,19 @@ public class Item {
         return description;
     }
 
-    public int getPrice() {
-        return price;
+    public int getStartedPrice() {
+        return startedPrice;
     }
 
-    public int getIdBuyer() {
+    public int getCurrentPrice() {
+        return startedPrice;
+    }
+
+    public String getIdBuyer() {
         return idBuyer;
     }
 
-    public int getIdSeller() {
+    public String getIdSeller() {
         return idSeller;
     }
 
--- app/src/main/java/mah/bidme/model/User.java
+++ app/src/main/java/mah/bidme/model/User.java
@@ -3,17 +3,12 @@ package mah.bidme.model;
 import java.util.ArrayList;
 
 public class User {
-    private int id;
+    private String id;
     private String name;
-    private String mail;
-    private ArrayList<Integer> listBid;
     private ArrayList<Item> listItem;
 
-    public User(String name, String mail) {
+    public User(String name) {
         this.name = name;
-        this.mail = mail;
-        this.listBid = new ArrayList<Integer>();
-        this.listItem = new ArrayList<Item>();
     }
 
     /**
@@ -24,55 +19,43 @@ public class User {
 
     }
 
-    /**
+   /* *//**
      * Return the last bid placed by the user
      * @return int
-     */
+     *//*
     public int getLastBid(){
         return listBid.get(listBid.size() - 1);
-    }
+    }*/
 
-    /**
+/*    *//**
      * Create an item into the Firebase database
      * @param item
-     */
+     *//*
     public void createItem(Item item){
 
     }
 
-    /**
+    *//**
      * Modify an existing item in the database
      * @param item
-     */
+     *//*
     public void editItem(Item item){
 
     }
 
-    /**
+    *//**
      * Delete an item from the database
      * @param item
-     */
+     *//*
     public void deleteItem(Item item){
 
-    }
+    }*/
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public ArrayList<Integer> getListBid() {
-        return listBid;
-    }
-
-    public ArrayList<Item> getListItem() {
-        return listItem;
     }
 }
